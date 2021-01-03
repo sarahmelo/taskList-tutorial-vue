34bcd42a72cd0fd346d3dab69c10e39d033f24a8
sarahmelo <sarahcbdg@gmail.com>
2021-01-03 19:06:48 -0300

    Add|Remove|Marking

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -10,31 +10,51 @@
       </div>
     </header>
     <div class="card-content">
-      <div class="content"></div>
-      <div class="content"></div>
+      <div class="content">
+        <novo-todo @novaTarefa="adicionarTarefa"></novo-todo>
+      </div>
+      <div class="content">
+        <todo-list :tarefas="tarefas" @check="checkTarefa" @remover="removerTarefa"></todo-list>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import NovoTodo from './NovoTodo'
+import TodoList from './TodoList'
 export default {
   name: 'todo-card',
+  components: {
+    NovoTodo,
+    TodoList
+  },
   data () {
     return {
-      dias : ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sexta','Sexta'],
-      meses : ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-      tarefas : []
-
+      dias: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+      meses: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+      tarefas: []
     }
   },
-    computed : {
-      hoje : function() {
-        let novaData = new Date() 
-        return `${this.dias[novaData.getDay()]}, ${novaData.getDate()} de ${this.meses[novaData.getMonth]}`
-      }
+  computed: {
+    hoje: function() {
+      let novaData = new Date()
+      return `${this.dias[novaData.getDay()]}, ${novaData.getDate()} de ${this.meses[novaData.getMonth()]}`
     }
+  },
+  methods: {
+    adicionarTarefa(tarefa) {
+      let nova_tarefa = {'description': tarefa, 'checked': false}
+      this.tarefas.push(nova_tarefa)
+    },
+    checkTarefa(index) {
+      this.tarefas[index]['checked'] = !this.tarefas[index]['checked']
+    },
+    removerTarefa(index){
+      this.tarefas.splice(index,1)
+    }
+  }
 }
-
 </script>
 
 <style>
@@ -45,3 +65,4 @@ export default {
   color: #636368;
 }
 </style>
+view rawTodoCard.vue hosted with ❤ by GitHub
